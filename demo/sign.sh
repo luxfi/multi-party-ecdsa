e56aff19ef26b7c7c80f66e6d510e42ecb510ab5
venuswhispers <venuswhisperer412@gmail.com>
2024-08-17 10:00:01 +0900

    update: demos

--- demo/sign.sh
+++ demo/sign.sh
@@ -10,22 +10,9 @@ echo "Multi-party ECDSA parties:$n threshold:$t"
 #clean
 sleep 1
 
-rm keys?.store
 killall gg18_sm_manager gg18_keygen_client gg18_sign_client 2> /dev/null
 
 ./target/release/examples/gg18_sm_manager &
-
-sleep 2
-echo "keygen part"
-
-for i in $(seq 1 $n)
-do
-    echo "key gen for client $i out of $n"
-    ./target/release/examples/gg18_keygen_client http://127.0.0.1:8001 keys$i.store &
-    sleep 3
-done
-
-
 
 sleep 5
 echo "sign"
